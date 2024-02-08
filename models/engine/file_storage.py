b5b511197294d4bd538a75a94d7c02d848f396cc
Azara <yyushahu02@gmail.com>
2024-02-08 10:23:15 +0000

    add string doc and rectify indentation with next logical line

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,14 +13,25 @@ class FileStorage():
     """
     __file_path = 'file.json'
     __objects = {}
+
     def all(self):
+        """A public instance method to the storage class
+        Returns:
+            the dictionary '__objects'
+        """
         return self.__objects
 
     def new(self, obj):
+        """A public instance method to the storage class that sets
+        inside of '__objects', the 'obj' with key <obj class name>.id
+        """
         key = f'{obj.__class__.__name__}.{obj.id}'
         self.__objects[key] = obj
 
     def save(self):
+        """A public instance method to the storage class that serialises
+        '__objects' to the JSON file
+        """
         serialised_objs = {}
         for key, value in self.__objects.items():
             serialised_objs[key] = value.to_dict()
@@ -28,10 +39,12 @@ class FileStorage():
             json.dump(serialised_objs, json_file)
 
     def reload(self):
+        """A public instance method to the storage class that deserialises
+        the JSON file to '__objects' if the JSON file exists."""
         try:
             with open(
                 self.__file_path, 'r', encoding='UTF-8'
-                ) as json_file:
+            ) as json_file:
                 loaded_objs = json.load(json_file)
                 for key, value in loaded_objs.items():
                     cls = value['__class__']

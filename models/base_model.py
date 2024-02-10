1cdd844d6f1517c91778c9eac81e4792cc1f5262
Azara <yyushahu02@gmail.com>
2024-02-10 22:37:09 +0000

    pycode check on basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,9 @@ class BaseModel():
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        value = datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f'
+                            )
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())

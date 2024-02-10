5f37521367306b3ad2b55479240cdce28d3a61f5
Azara <yyushahu02@gmail.com>
2024-02-10 22:33:38 +0000

    make executables

--- models/base_model.py
+++ models/base_model.py
@@ -48,7 +48,5 @@ class BaseModel():
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat() if hasattr(
-            self, 'updated_at'
-        ) else None
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
